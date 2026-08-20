7465a8f22e5849e4601c290aa5e1d67edee2f73c
Willa Evangelista <willaevangelista.pro@gmail.com>
2026-08-20 18:53:54 -0300

    feat: add global exception handling with CustomError

---
+++ src/main/java/com/willaevangelista/dscommerce/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.willaevangelista.dscommerce.controllers;
+
+import com.willaevangelista.dscommerce.dto.CustomError;
+import com.willaevangelista.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
